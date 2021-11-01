678df75e7b4ccd51f7f2dea91cc6c5c7d8a95c9b
theringsofsaturn <emiliankasemi@gmail.com>
2021-11-01 19:37:50 +0100

    mongoose user model added

--- api/index.js
+++ api/index.js
@@ -8,9 +8,11 @@ const port = 3001;
 const server = express();
 
 // mongoose getting-started.js
+main().catch(err => console.log(err))
+
 async function main() {
   await mongoose.connect(process.env.MONGO_URL);
-}.then(() => console.log("DB is running!"))
+}
 
 server.listen(port, () => {
   console.log("🧡 server is running on port: " + port);
