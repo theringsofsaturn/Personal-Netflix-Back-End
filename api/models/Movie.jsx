6518f3f50e2f125603ca3c433d0bfc8395af7650
theringsofsaturn <emiliankasemi@gmail.com>
2021-11-01 19:52:57 +0100

    mongoose movie schema model added

---
+++ api/models/Movie.jsx
@@ -0,0 +1,17 @@
+import mongoose, { model } from "mongoose";
+
+const MovieSchema = new mongoose.Schema({
+  title: { type: "String", required: true },
+  desc: { type: "String" },
+  img: { type: "String" },
+  imgTitle: { type: "String" },
+  imgSm: { type: "String" },
+  trailer: { type: "String" },
+  video: { type: "String" },
+  year: { type: "String" },
+  limit: { type: "String" },
+  genre: { type: "String" },
+  series: { type: Boolean, required: false },
+});
+
+model.exports = mongoose.model("Movie", MovieSchema)
