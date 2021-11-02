adbbb810ef69ae4fd69831b93e4575cceda34fc6
theringsofsaturn <emiliankasemi@gmail.com>
2021-11-02 01:50:32 +0100

    register post method added

--- api/index.js
+++ api/index.js
@@ -1,11 +1,18 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import listEndpoints from "express-list-endpoints"
+import authRouter from "./routes/auth.js"
 
 dotenv.config();
 
 const port = 3001;
 const server = express();
+server.use(express.json())
+
+/* ************ENDPOINTS******************* */
+
+server.use("/api/auth", authRouter)
 
 // mongoose getting-started.js
 main().catch((err) => console.log(err));
@@ -14,6 +21,7 @@ async function main() {
   await mongoose.connect(process.env.MONGO_URL);
 }
 
+console.table(listEndpoints(server));
 server.listen(port, () => {
   console.log("🧡 server is running on port: " + port);
 });
--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,10 +1,22 @@
-import User from ("../models/User");
+import express from "express";
+import User from "../models/User.js";
 
-const router = express.Router()
+const authRouter = express.Router();
 
 //REGISTER
-router.post('/register', (req, res) => {
-    const newUser = new User({
-        username: req.body.username,
-    })
-})
+authRouter.post("/register", async (req, res) => {
+  const newUser = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+  });
+  try {
+    const user = await newUser.save();
+    res.json(user);
+  } catch (error) {
+      console.log(error)
+      res.status(500).json(error);
+  }
+});
+
+export default authRouter;
