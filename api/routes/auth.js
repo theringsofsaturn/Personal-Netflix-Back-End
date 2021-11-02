e9ca5ceb20a3e8a5c58072cce939b88f0286b60f
theringsofsaturn <emiliankasemi@gmail.com>
2021-11-02 14:18:50 +0100

    minor changes. still error on Login

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -9,6 +9,7 @@ authRouter.post("/register", async (req, res) => {
   const newUser = new User({
     username: req.body.username,
     email: req.body.email,
+    // crypto-js packace script to encrypt password.
     password: CryptoJS.AES.encrypt(
       "req.body.password",
       process.env.SECRET_KEY
@@ -26,9 +27,12 @@ authRouter.post("/register", async (req, res) => {
 // LOGIN
 authRouter.post("/login", async (req, res) => {
   try {
+    // find the user. if email = request (req.body.email)
     const user = await User.findOne({ email: req.body.email });
+    // if there is no user with that email send a status error
     !user && res.status(401).json("Wrong password or username!");
 
+    // crypto-js script to decrypt password
     const bytes = CryptoJS.AES.decrypt(user.password, process.env.SECRET_KEY);
     const originalPassword = bytes.toString(CryptoJS.enc.Utf8);
 
