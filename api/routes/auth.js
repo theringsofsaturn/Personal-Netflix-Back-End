79ad56bc4d692586f4bb6e82b9b8456cea72f991
theringsofsaturn <emiliankasemi@gmail.com>
2021-11-02 02:53:55 +0100

    Added Login but having some issues

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -23,4 +23,23 @@ authRouter.post("/register", async (req, res) => {
   }
 });
 
+// LOGIN
+authRouter.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    !user && res.status(401).json("Wrong password or username!");
+
+    const bytes = CryptoJS.AES.decrypt(user.password, process.env.SECRET_KEY);
+    const originalPassword = bytes.toString(CryptoJS.enc.Utf8);
+
+    originalPassword !== req.body.password &&
+      res.status(401).json("Wrong password or username!");
+
+    res.status(200).json(user);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+});
+
 export default authRouter;
