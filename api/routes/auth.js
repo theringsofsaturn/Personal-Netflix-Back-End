d5af67f4d935dee97a28da49c5433cbfbb3af54c
theringsofsaturn <emiliankasemi@gmail.com>
2021-11-02 02:18:41 +0100

    encrypted password with crypto-js

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,5 +1,6 @@
 import express from "express";
 import User from "../models/User.js";
+import CryptoJS from "crypto-js";
 
 const authRouter = express.Router();
 
@@ -8,14 +9,17 @@ authRouter.post("/register", async (req, res) => {
   const newUser = new User({
     username: req.body.username,
     email: req.body.email,
-    password: req.body.password,
+    password: CryptoJS.AES.encrypt(
+      "req.body.password",
+      process.env.SECRET_KEY
+    ).toString(),
   });
   try {
     const user = await newUser.save();
     res.json(user);
   } catch (error) {
-      console.log(error)
-      res.status(500).json(error);
+    console.log(error);
+    res.status(500).json(error);
   }
 });
 
