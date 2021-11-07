39468389feae5239f314892e5257ba83bc4582dd
theringsofsaturn <emiliankasemi@gmail.com>
2021-11-08 00:28:32 +0100

    UPDATE movie

--- api/routes/movies.js
+++ api/routes/movies.js
@@ -9,6 +9,7 @@ movieRouter.post("/", verify, async (req, res) => {
   if (req.user.isAdmin) {
     const newMovie = new Movie(req.body); // new movie model with whate we got from the request
     try {
+      // Save movie to database
       const savedMovie = await newMovie.save();
       res.status(201).json(savedMovie);
     } catch (err) {
@@ -19,6 +20,25 @@ movieRouter.post("/", verify, async (req, res) => {
   }
 });
 
-
+// ***************** UPDATE *****************
+movieRouter.put("/:id", verify, async (req, res) => {
+  if (req.user.isAdmin) {
+    try {
+        // Find movie by id that was sent in the request. After finding it, update it with the new data (set the new data in the body)
+      const updatedMovie = await Movie.findByIdAndUpdate(
+        req.params.id,
+        {
+          $set: req.body,
+        },
+        { new: true } //remember to add this congifuration to get the updated data, if not, it will return the old data
+      );
+      res.status(200).json(updatedMovie);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(403).json("You are not allowed!");
+  }
+});
 
 export default movieRouter;
