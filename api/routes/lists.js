ba749048dbf14bbd875a78949661fe01edb75f7a
theringsofsaturn <emiliankasemi@gmail.com>
2021-11-08 02:01:32 +0100

    DELETE lists

--- api/routes/lists.js
+++ api/routes/lists.js
@@ -1,23 +1,36 @@
 import express from "express";
-import Movie from "../models/Movie.js";
+import List from "../models/List.js";
 import verify from "../verifyToken.js";
 
 const listsRouter = express.Router();
 
 // ***************** CREATE *****************
 listsRouter.post("/", verify, async (req, res) => {
-    if (req.user.isAdmin) {
-      const newList = new List(req.body);
-      try {
-        const savedList = await newList.save();
-        res.status(201).json(savedList);
-      } catch (err) {
-        res.status(500).json(err);
-      }
-    } else {
-      res.status(403).json("You are not allowed!");
+  if (req.user.isAdmin) {
+    const newList = new List(req.body);
+    try {
+      const savedList = await newList.save();
+      res.status(201).json(savedList);
+    } catch (err) {
+      res.status(500).json(err);
     }
-  });
+  } else {
+    res.status(403).json("You are not allowed!");
+  }
+});
 
+// ***************** DELETE *****************
+listsRouter.delete("/:id", verify, async (req, res) => {
+  if (req.user.isAdmin) {
+    try {
+      await List.findByIdAndDelete(req.params.id);
+      res.status(201).json("The list has been delete...");
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(403).json("You are not allowed!");
+  }
+});
 
 export default listsRouter;
